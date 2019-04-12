be9d0661c795a6110d0ff5495697e84b8f5aeb58
Ricardo Barata <ricardo.barata@gmail.com>
2019-04-13 00:39:28 +0100

    Remove update secrets code

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-TAG     := $(shell git describe --tags --abbrev=0 HEAD)
+TAG     := v0.1.0
 PKGS    := $(shell go list ./... | grep -v /vendor/)
-PREFIX  := quay.io/roboll
+PREFIX  := talkdesk
 
 generate:
 	go generate ${PKGS}
--- pkg/controller/secret_claim_handler.go
+++ pkg/controller/secret_claim_handler.go
@@ -32,50 +32,6 @@ func newSecretClaimHandler(manager kube.SecretClaimManager) cache.ResourceEventH
 				log.Printf("error: failed to update secret for key %s: %s", key, err.Error())
 			}
 		},
-		UpdateFunc: func(old, obj interface{}) {
-			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-			if err != nil {
-				log.Printf("error: failed to get key for obj, dropping.")
-				return
-			}
-
-			log.Printf("secret-claim-handler: %s: handling update for secret claim", key)
-			claim, ok := obj.(*kube.SecretClaim)
-			if !ok {
-				log.Printf("expected *kube.SecretClaim, got %s", reflect.TypeOf(obj))
-				return
-			}
-			oldClaim, ok := old.(*kube.SecretClaim)
-			if !ok {
-				log.Printf("expected *kube.SecretClaim, got %s", reflect.TypeOf(old))
-				return
-			}
-
-			force := !reflect.DeepEqual(claim.Spec, oldClaim.Spec)
-			log.Printf("secret-claim-handler: %s: scheduling create/update for secret (force=%t)", key, force)
-			if err := manager.CreateOrUpdateSecret(claim, force); err != nil {
-				log.Printf("error: failed to update secret for key %s: %s", key, err.Error())
-			}
-		},
-		DeleteFunc: func(obj interface{}) {
-			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-			if err != nil {
-				log.Printf("error: failed to get key for obj, dropping.")
-				return
-			}
-
-			log.Printf("secret-claim-handler: %s: handling delete for secret claim", key)
-			claim, ok := obj.(*kube.SecretClaim)
-			if !ok {
-				log.Printf("error: expected *kube.SecretClaim, got %s", reflect.TypeOf(obj))
-				return
-			}
-
-			log.Printf("secret-claim-handler: %s: scheduling delete for secret", key)
-			if err := manager.DeleteSecret(claim); err != nil {
-				log.Printf("error: failed to delete secret for key %s: %s", key, err.Error())
-			}
-		},
 	}
 }
 
